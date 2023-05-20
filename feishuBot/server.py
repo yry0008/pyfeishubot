529970070ffe5a1fe496602448987dab937f8ce2
Yang <76504564+yry0008@users.noreply.github.com>
2023-05-20 18:33:54 +0000

    modified:   feishuBot/server.py

--- feishuBot/server.py
+++ feishuBot/server.py
@@ -5,6 +5,31 @@ import uvicorn
 from fastapi import FastAPI, Form , Header , Request , Response
 from feishuBot import utils,route as fastapi_route
 import json
+import ctypes
+import inspect
+
+def _async_raise(tid, exctype):
+    """raises the exception, performs cleanup if needed"""
+    try:
+        tid = ctypes.c_long(tid)
+        if not inspect.isclass(exctype):
+            exctype = type(exctype)
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
+        if res == 0:
+            # pass
+            raise ValueError("invalid thread id")
+        elif res != 1:
+            # """if it returns a number greater than one, you're in trouble,
+            # and you should call it again with exc=NULL to revert the effect"""
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, None)
+            raise SystemError("PyThreadState_SetAsyncExc failed")
+    except Exception as err:
+        print(err)
+
+def _stop_thread(thread):
+    """终止-线程"""
+    _async_raise(thread.ident, SystemExit)
+
 class run_backend(threading.Thread):
     """
     The Feishu Webhook server backend, powered by FastAPI.
@@ -85,5 +110,8 @@ class run_backend(threading.Thread):
 
     def run(self):
         uvicorn.run(self.fastapi,host=self.bind,port=self.bind_port)
+
+    def stop(self):
+        _stop_thread(self)
 
     
